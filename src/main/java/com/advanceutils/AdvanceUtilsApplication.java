343bd074a9b6c61eb0895e67feb8a6ce61099c04
GARTNER\sbansal <saransh.bansal@gartner.com>
2018-02-25 13:02:19 +0530

    spring-boot setup

---
+++ src/main/java/com/advanceutils/AdvanceUtilsApplication.java
@@ -0,0 +1,12 @@
+package com.advanceutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceUtilsApplication.class, args);
+	}
+}
